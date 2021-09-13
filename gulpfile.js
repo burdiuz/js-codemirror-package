d71229bcc95efa83b4d53e703e018c4816117fd6
Oleg Galaburda <burdiuz@gmail.com>
2021-09-13 19:20:54 +0300

    fix gulp fatal incompatibility with newer node versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const gulp = require('gulp');
+const { src, dest, series } = require('gulp');
 const uglify = require('gulp-uglify');
 const gulpif = require('gulp-if');
 const gutil = require('gulp-util');
@@ -30,15 +30,14 @@ const putDataInto = (path, data, target = {}) => {
 
 const loadDataPath = (path, data) => {
   return new Promise((res, rej) => {
-    gulp
-      .src(path)
+    src(path)
       .pipe(gulpif(stream => stream.path.match(/\.css$/), css()))
       .pipe(gulpif(stream => stream.path.match(/\.js$/), uglify()))
       .on("error", function(err) {
         gutil.log(gutil.colors.red("[Error]"), err.toString());
       })
       .pipe(createStream(data))
-      .pipe(gulp.dest('./tmp/'))
+      .pipe(dest('./tmp/'))
       .on('end', () => {
         res(data);
       });
@@ -81,7 +80,11 @@ const createBundle = (fileName, paths) => {
   });
 };
 
-  gulp.task('editor', () => createBundle('editor', ['lib']));
-  gulp.task('themes', () => createBundle('themes', ['theme']));
-  gulp.task('modules', () => createBundle('modules', ['addon', 'keymap', 'mode']));
-  gulp.task('default', ['editor', 'themes', 'modules']);
+const editor = () => createBundle('editor', ['lib']);
+const themes = () => createBundle('themes', ['theme']);
+const modules = () => createBundle('modules', ['addon', 'keymap', 'mode']);
+
+exports.editor = editor;
+exports.themes = themes;
+exports.modules = modules;
+exports.default = series(editor, themes, modules);
